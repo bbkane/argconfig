7b9e37ec4351912e633e21e20e7ab665a7ab0cc7
Benjamin Kane <bbk1524@gmail.com>
2017-08-28 21:25:42 -0500

    move to pip friendly structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='argconfig',
+      version='0.0.1',
+      description='Use config files with argparse',
+      url='https://github.com/bbkane/argconfig',
+      author='Benjamin Kane',
+      author_email='bbk1524@gmail.com',
+      license='MIT',
+      packages=['argconfig'],
+      zip_safe=False)
